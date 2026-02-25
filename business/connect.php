87725b338919e001da81a62a60ab2c8eef779061
koa8845 <684259037@webmail.npru.ac.th>
2026-02-25 16:46:03 +0700

    Add files via upload

---
+++ business/connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$userName = "root";
+$userPassword = "";
+$dbname ="business";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $userName, $userPassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
